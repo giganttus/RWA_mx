71048158856dc7d83b191bcfa61e4069a4e2a3a9
giganttus <domagojpr@gmail.com>
2020-01-20 15:45:47 +0100

    Last patch

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\User;
+use App\Role;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -69,7 +70,7 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
         ]);
 
-        $role = Role::select('id')->where('name', 'user');
+        $role = Role::select('id')->where('name', 'korisnik')->first();
 
         $user->roles()->attach($role);
 
